fd71bf66d7dfbc4a1817ed837e4cc98dc5590edb
arsaizdihar <arsadihar@gmail.com>
2023-02-03 22:22:51 +0700

    Add missing enums

--- src/main/java/Enums/PlayerActions.java
+++ src/main/java/Enums/PlayerActions.java
@@ -1,14 +1,20 @@
 package Enums;
 
 public enum PlayerActions {
-  FORWARD(1),
-  STOP(2),
-  START_AFTERBURNER(3),
-  STOP_AFTERBURNER(4);
+    FORWARD(1),
+    STOP(2),
+    START_AFTERBURNER(3),
+    STOP_AFTERBURNER(4),
+    FIRE_TORPEDOES(5),
+    FIRE_SUPERNOVA(6),
+    DETONATE_SUPERNOVA(7),
+    FIRE_TELEPORTER(8),
+    TELEPORT(9),
+    USE_SHIELD(10);
 
-  public final Integer value;
+    public final Integer value;
 
-  private PlayerActions(Integer value) {
-    this.value = value;
-  }
+    private PlayerActions(Integer value) {
+        this.value = value;
+    }
 }

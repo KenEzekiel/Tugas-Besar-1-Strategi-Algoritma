38c3af76fd3ec5d44bdce6df9c7eb1d28f46c024
arsaizdihar <arsadihar@gmail.com>
2023-02-04 00:21:40 +0700

    Move math functionality

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -1,10 +1,12 @@
 package Services;
 
-import Enums.*;
-import Models.*;
+import Enums.PlayerActions;
+import Models.GameObject;
+import Models.GameState;
+import Models.PlayerAction;
 
-import java.util.*;
-import java.util.stream.*;
+import java.util.Optional;
+import java.util.Random;
 
 public class BotService {
     private GameObject bot;
@@ -37,16 +39,6 @@ public class BotService {
         playerAction.action = PlayerActions.FORWARD;
         playerAction.heading = new Random().nextInt(360);
 
-        if (!gameState.getGameObjects().isEmpty()) {
-            var foodList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
-                    .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
-                    .collect(Collectors.toList());
-
-            playerAction.heading = getHeadingBetween(foodList.get(0));
-        }
-
         this.playerAction = playerAction;
     }
 
@@ -62,22 +54,6 @@ public class BotService {
     private void updateSelfState() {
         Optional<GameObject> optionalBot = gameState.getPlayerGameObjects().stream().filter(gameObject -> gameObject.id.equals(bot.id)).findAny();
         optionalBot.ifPresent(bot -> this.bot = bot);
-    }
-
-    private double getDistanceBetween(GameObject object1, GameObject object2) {
-        var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
-        var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
-        return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
-    }
-
-    private int getHeadingBetween(GameObject otherObject) {
-        var direction = toDegrees(Math.atan2(otherObject.getPosition().y - bot.getPosition().y,
-                otherObject.getPosition().x - bot.getPosition().x));
-        return (direction + 360) % 360;
-    }
-
-    private int toDegrees(double v) {
-        return (int) (v * (180 / Math.PI));
     }
 
 

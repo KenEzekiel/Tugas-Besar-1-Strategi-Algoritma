4ac43add7ee37aa563f3e86c44b0d38ee688190a
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-02-17 00:19:24 +0700

    modded TorpedoProcessorObstacle to be more precise and specific

--- src/main/java/Processors/TorpedoProcessorObstacle.java
+++ src/main/java/Processors/TorpedoProcessorObstacle.java
@@ -5,14 +5,15 @@ import Enums.PlayerActions;
 import Models.ActionWeight;
 import Models.GameObject;
 import Models.GameState;
+import Models.Position;
 import Services.MathService;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class TorpedoProcessorObstacle extends Processor {
-    final static double MINVAL = 16.0;
-    final static double VALUE = 5.0;
+    final static double MINVAL = 20.0;
+    final static double thresholdDistance = 5.0;
 
     public TorpedoProcessorObstacle(GameObject bot, GameState gameState) {
         super(bot, gameState);
@@ -22,47 +23,48 @@ public class TorpedoProcessorObstacle extends Processor {
     public void process() {
         var botPos = bot.getProjectedPosition();
         var gasCloudList = gameState.getGameObjects()
-                .stream().filter(item -> item.getGameObjectType() == ObjectTypes.GAS_CLOUD)
+                .stream().filter(item -> item.getGameObjectType() == ObjectTypes.GAS_CLOUD && MathService.getDistanceBetween(bot, item) < 20)
 //                .sorted(Comparator
 //                        .comparing(item -> MathService.getDistanceBetween(bot, item)))
                 .collect(Collectors.toList());
         var ActionHeadingList = new ArrayList<ActionWeight>();
-        PlayerActions actionTaken = PlayerActions.Forward;
-        double worldDiameter = gameState.getWorld().radius * 2;
+        PlayerActions actionTaken = PlayerActions.FireTorpedoes;
+        double worldRadius = gameState.getWorld().radius;
 
 
         // Only trigger if the player has more than some number MINVAL
-        // Weight will be + if player's salvo is nearing max
         for (GameObject obj : gasCloudList) {
             if (bot.getSize() > MINVAL && bot.torpedoSalvoCount > 0) {
                 int heading = MathService.getHeadingBetween(bot, obj);
-                var distance = MathService.getDistanceBetween(botPos, obj.getPosition()) - bot.getSize() - obj.getSize();
-                if (distance > 100) {
-                    continue;
-                }
-                double distanceValue = MathService.getDistanceBetween(bot, obj) != 0 ? (worldDiameter - distance) / worldDiameter : 5;
-                // 1.2 and 0.3 is still an arbitrary value
-//                double headingValue = bot.currentHeading == heading ? 1.2 : 0.3;
-                double groupValue;
-
-                // Can be changed, how near, how grouped, how big
-                double weight;
+                double weight = 0;
 
                 boolean biggerThanHalf = obj.getSize() > bot.getSize() * 0.5;
-                if (biggerThanHalf || bot.getSize() <= MINVAL || bot.torpedoSalvoCount == 0) {
-                    actionTaken = PlayerActions.Forward;
-                    heading = MathService.reverseHeading(heading);
-                    weight = distanceValue;
-                } else {
-                    actionTaken = PlayerActions.FireTorpedoes;
-                    weight = VALUE * distanceValue;
-                }
+                // v = d/t
+                // v : bot.speed    t -> per tick -1 size -> t <= 0.5 * bot.size (keep half of the size after traversing the cloud)
+                // t = d/v <= 0.5 * bot.size
+                boolean canHead = obj.getSize() * 2 / bot.getSpeed() <= 0.5 * bot.getSize();
+                boolean canDestroy = !biggerThanHalf && obj.getSize() < 15;
 
+                var worldCenter = new Position(0, 0);
+                double distanceToCenter = MathService.getDistanceBetween(botPos, worldCenter);
+
+                if (worldRadius - distanceToCenter > thresholdDistance && heading == MathService.getHeadingBetween(bot.getPosition(), worldCenter)) {
+                    if (canHead) {
+                        actionTaken = PlayerActions.Forward;
+                        weight = 15;
+                        heading = MathService.getHeadingBetween(bot.getPosition(), worldCenter);
+                    } else if (canDestroy) {
+                        weight = 15;
+                    }
+                } else {
+                    weight = 2;
+                }
 
                 var actionWeight = new ActionWeight(heading, weight);
                 ActionHeadingList.add(actionWeight);
+                this.data.put(actionTaken, ActionHeadingList);
             }
-            this.data.put(actionTaken, ActionHeadingList);
+
         }
     }
 }

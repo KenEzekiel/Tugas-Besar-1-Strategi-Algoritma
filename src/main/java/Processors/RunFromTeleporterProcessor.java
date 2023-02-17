2e181872198c54e8757c85d8d6167c3d58ff6e2b
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-02-17 21:32:32 +0700

    Make more sensitive to teleporter

--- src/main/java/Processors/RunFromTeleporterProcessor.java
+++ src/main/java/Processors/RunFromTeleporterProcessor.java
@@ -26,7 +26,7 @@ public class RunFromTeleporterProcessor extends Processor{
             return;
         }
         var tlpList = gameState.getGameObjects().stream()
-                .filter(item -> item.gameObjectType == ObjectTypes.TELEPORTER && item.getCurrentHeading() == MathService.getHeadingBetween(item, bot) && MathService.getDistanceBetween(bot, item) < 50)
+                .filter(item -> item.gameObjectType == ObjectTypes.TELEPORTER && MathService.getDegreeDifference(item.getCurrentHeading(), MathService.getHeadingBetween(item, bot)) < 10 && MathService.getDistanceBetween(bot, item) < 50)
                 .collect(Collectors.toList());
 
         var ActionHeadingList = new ArrayList<ActionWeight>();
